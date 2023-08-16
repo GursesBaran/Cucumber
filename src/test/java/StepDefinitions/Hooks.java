d5e149c0689fa5a08adfde13d93ad09f08804bcd
Baran Gurses <barangurses39@hotmail.com>
2023-08-16 13:52:52 -0400

    Cucumber

--- src/test/java/StepDefinitions/Hooks.java
+++ src/test/java/StepDefinitions/Hooks.java
@@ -1,26 +1,34 @@
 package StepDefinitions;
 
-import Utilities.BaseDriver;
+import Utilities.Excel;
 import Utilities.ParameterDriver;
 import io.cucumber.java.*;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 public class Hooks {
+    LocalDateTime startTime;
+    LocalDateTime endTime;
 
     @Before
     public void beforeScenario() {
+        startTime = LocalDateTime.now();
         System.out.println("Before Scenario");
     }
 
     @After
     public void afterScenario(Scenario scenario) {
-        if (scenario.isFailed()) {
-            final byte[] byteImage = ((TakesScreenshot) BaseDriver.getDriver()).getScreenshotAs(OutputType.BYTES);
+        endTime = LocalDateTime.now();
+        Duration duration = Duration.between(startTime,endTime);
+        if (scenario.isFailed()) { // take screen shot when the scenario fails
+            final byte[] byteImage = ((TakesScreenshot) ParameterDriver.getDriver()).getScreenshotAs(OutputType.BYTES);
             scenario.attach(byteImage, "image/png", scenario.getName());
-
-
         }
+        Excel.writeInExcel("src/test/java/ApachePOI/Resources/CampusScenarioResults.xlsx", scenario,startTime, endTime,duration);
+        ParameterDriver.quitDriver();
+
 
         ParameterDriver.quitDriver();
 
